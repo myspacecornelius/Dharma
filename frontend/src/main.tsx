f4c8a459403bb9c6c8cbab87223bb42bed60c019
David Nichols <david.c.nichols50@gmail.com>
2025-08-22 21:06:58 -0400

    changes to frontend container configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './globals.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from './components/ThemeProvider'
 import { Toaster } from 'sonner'
-import router from './routes'
+import { ThemeProvider } from './components/ThemeProvider'
 import './index.css'
+import router from './routes'
 
 const queryClient = new QueryClient()
 
