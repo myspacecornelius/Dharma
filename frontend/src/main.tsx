a6efbdb16a2b3da97d5882dd89064f27c1548988
myspacecornelius <david.c.nichols50@gmail.com>
2025-08-24 01:42:31 -0400

    sos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Toaster } from 'sonner'
-import { ThemeProvider } from './components/ThemeProvider'
-import './index.css'
 import router from './routes'
+import './styles/global.css'
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-        <Toaster />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
